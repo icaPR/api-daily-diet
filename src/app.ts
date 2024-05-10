70436296c9e96f57817fb60d568371351054a669
Icaro Ramos <polosnramos@hotmail.com>
2024-05-09 22:29:56 -0300

    feat: Register cookie plugin in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import fastify from "fastify";
+import cookie from "@fastify/cookie";
+
 import { users } from "./routes/users";
 
 export const app = fastify();
+app.register(cookie);
 
 app.register(users, { prefix: "users" });

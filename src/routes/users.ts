d4f90b0f843adb5592ae98ddee40f2279ad215d6
Icaro Ramos <polosnramos@hotmail.com>
2024-05-10 04:00:08 -0300

    feat: Add /metric route and save cookie in users

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -14,6 +14,16 @@ export async function users(app: FastifyInstance) {
 
     const body = createUserBodyScheme.parse(req.body);
 
+    let user_id = req.cookies.user_id;
+    if (!user_id) {
+      user_id = randomUUID();
+
+      res.cookie("user_id", user_id, {
+        path: "/",
+        maxAge: 60 * 60 * 24, // 1 day
+      });
+    }
+
     await knex("users").insert({
       id: randomUUID(),
       name: body.name,
@@ -37,5 +47,43 @@ export async function users(app: FastifyInstance) {
 
     const users = await knex("users").where({ id }).first();
     return { users };
+  });
+
+  app.get("/metric", async (req, res) => {
+    const user_id = req.cookies.user_id;
+
+    const meals = await knex("meals").orderBy("user_id").orderBy("created_at");
+
+    let longestSequence = 0;
+    let currentSequence = 0;
+    let currentUser = null;
+
+    for (const meal of meals) {
+      if (meal.user_id !== currentUser) {
+        currentSequence = 0;
+        currentUser = meal.user_id;
+      }
+      if (meal.be_on_the_diet) {
+        currentSequence++;
+        longestSequence = Math.max(longestSequence, currentSequence);
+      } else {
+        currentSequence = 0;
+      }
+    }
+
+    const metric = {
+      totalMeals: await knex("meals").count().first(),
+      totalDietMeals: await knex("meals")
+        .where({ be_on_the_diet: true, user_id })
+        .count()
+        .first(),
+      totalNoDietMeals: await knex("meals")
+        .where({ be_on_the_diet: false, user_id })
+        .count()
+        .first(),
+      bestDietMealsSequence: longestSequence,
+    };
+
+    return { metric };
   });
 }
